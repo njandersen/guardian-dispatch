6037496c4a73aabcb7d23f2eaefde2bba8f20361
njandersen <njandersen22@gmail.com>
2022-10-04 10:04:46 -0400

    set up the admin pages

---
+++ pages/admin/[slug].js
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { useDocumentDataOnce } from "react-firebase-hooks/firestore";
+import { useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
+import Link from "next/link";
+import toast, { Toast } from "react-hot-toast";
+import {
+  getFirestore,
+  doc,
+  updateDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+
+import AuthCheck from "../../components/AuthCheck";
+import { auth } from "../../lib/firebase";
+
+import styles from "./AdminPostsPage.module.scss";
+
+export default function AdminPostEdit(props) {
+  return (
+    <AuthCheck>
+      <PostManager />
+    </AuthCheck>
+  );
+}
+
+function PostManager() {
+  const [preview, setPreview] = useState(false);
+
+  const router = useRouter();
+  const { slug } = router.query;
+
+  const postRef = doc(
+    getFirestore(),
+    "users",
+    auth.currentUser.uid,
+    "posts",
+    slug
+  );
+  const [post] = useDocumentDataOnce(postRef);
+
+  return (
+    <main className={styles.container}>
+      {post && (
+        <>
+          <section>
+            <h1>{post.title}</h1>
+            <p>ID: {post.slug}</p>
+
+            <PostForm
+              postRef={postRef}
+              defaultValues={post}
+              preview={preview}
+            />
+          </section>
+
+          <aside>
+            <h3>Tools</h3>
+            <button onClick={() => setPreview(!preview)}>
+              {preview ? "Edit" : "Preview"}
+            </button>
+            <Link href={`/${post.username}/${post.slug}`}>
+              <button>Live view</button>
+            </Link>
+          </aside>
+        </>
+      )}
+    </main>
+  );
+}
+
+function PostForm({ defaultValues, postRef, preview }) {
+  const { register, handleSubmit, reset, watch } = useForm({
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const updatePost = async ({ content, published }) => {
+    await updateDoc(postRef, {
+      content,
+      published,
+      updatedAt: serverTimestamp(),
+    });
+
+    reset({ content, published });
+    toast.success("Post updated successfully");
+  };
+
+  return (
+    <form onSubmit={handleSubmit(updatePost)}>
+      {preview && (
+        <div className={styles.formCard}>
+          <ReactMarkdown>{watch("content")}</ReactMarkdown>
+        </div>
+      )}
+
+      <div className={preview ? styles.hidden : styles.controls}>
+        <textarea
+          name="content"
+          {...register("content", { required: true })}
+        ></textarea>
+
+        <fieldset>
+          <input
+            className={styles.checkbox}
+            name="published"
+            type="checkbox"
+            {...register("published", { required: true })}
+          />
+          <label>Published</label>
+        </fieldset>
+
+        <button type="submit" className={styles.btnSave}>
+          Save Changes
+        </button>
+      </div>
+    </form>
+  );
+}
---
+++ pages/admin/index.js
@@ -0,0 +1,108 @@
+import {
+  serverTimestamp,
+  query,
+  collection,
+  orderBy,
+  getFirestore,
+  setDoc,
+  doc,
+} from "firebase/firestore";
+
+import { useContext, useState } from "react";
+import { useRouter } from "next/router";
+import { useCollection } from "react-firebase-hooks/firestore";
+import kebabCase from "lodash.kebabcase";
+import toast from "react-hot-toast";
+
+import AuthCheck from "../../components/AuthCheck";
+import PostFeed from "../../components/PostFeed";
+import { firestore, auth } from "../../lib/firebase";
+import { UserContext } from "../../lib/context";
+import styles from "./AdminPostsPage.module.scss";
+
+export default function AdminPostsPage(props) {
+  return (
+    <main className={styles.adminContainer}>
+      <AuthCheck>
+        <PostList />
+        <CreateNewPost />
+      </AuthCheck>
+    </main>
+  );
+}
+
+function PostList() {
+  const ref = collection(
+    getFirestore(),
+    "users",
+    auth.currentUser.uid,
+    "posts"
+  );
+  const postQuery = query(ref, orderBy("createdAt"));
+
+  const [querySnapshot] = useCollection(postQuery);
+
+  const posts = querySnapshot?.docs.map((doc) => doc.data());
+
+  return (
+    <>
+      <h1>Manage your Posts</h1>
+      <PostFeed posts={posts} admin />
+    </>
+  );
+}
+
+function CreateNewPost() {
+  const router = useRouter();
+  const { username } = useContext(UserContext);
+  const [title, setTitle] = useState("");
+
+  // Ensure slug is URL safe
+  const slug = encodeURI(kebabCase(title));
+
+  // Validate length
+  const isValid = title.length > 3 && title.length < 100;
+
+  // Create a new post in firestore
+  const createPost = async (e) => {
+    e.preventDefault();
+    const uid = auth.currentUser.uid;
+    const ref = doc(getFirestore(), "users", uid, "posts", slug);
+
+    const data = {
+      title,
+      slug,
+      uid,
+      username,
+      published: false,
+      content: "# hello world!",
+      createdAt: serverTimestamp(),
+      updatedAt: serverTimestamp(),
+      spicyRamenCount: 0,
+    };
+
+    await setDoc(ref, data);
+
+    toast.success("Post created!");
+
+    // Imperative navigation after doc is set
+    router.push(`/admin/${slug}`);
+  };
+
+  return (
+    <form className={styles.createForm} onSubmit={createPost}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="My Awesome Article!"
+        className={styles.title}
+      />
+      <p>
+        <strong>Slug:</strong> {slug}
+      </p>
+      <button type="submit" disabled={!isValid} className={styles.btnCreate}>
+        Create New Post
+      </button>
+    </form>
+  );
+}
